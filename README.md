8a11ff3c2951d4aadecbfef7ab818ef96b2c2806
HidayaSyam <hid.sy1910@gmail.com>
2020-09-13 19:08:33 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # data-structures-and-algorithms
+
+
+# table of contents
+
+| #num     |    Content     |    pull request | 
+|----------|:-----------:|:----------:|
+| day 1    |      Array Revers       |            |
+| day 2    |    Array Shift         |            |
+| day 3    |       Binary Search      |            |
+| day 4    |     Linked List        |            |
+| day 5    |             |            |
+| day 6    |             |            |
+| day 7    |             |            |
+| day 8    |             |            |
+| day 9    |             |            |
+| day 10   |             |            |
+| day 11   |             |            |
+| day 12   |             |            |
+| day 13   |             |            |
+| day 14   |             |            |
+| day 15   |             |            |
--- __test__/linked-list.test.js
+++ __test__/linked-list.test.js
@@ -1,65 +1,7 @@
 'use strict'
 
-class Node {
-  constructor(val) {
-    this.data = val;
-    this.next = null;
-  }
-}
-
-class linkedlist {
-
-  constructor() {
-    this.head = null;
-  }
-
-  insert(val) {
-    let node = new Node(val);
-    //console.log(node)
-    if (this.head === null) {
-      this.head = node;
-    }
-    else {
-      let current = this.head;
-      while (current.next !== null) {
-        current = current.next;
-        //console.log(current.data)
-      }
-      current.next = node;
-      //console.log(current.data)
-    }
-
-
-  }
-  includes(val){
-      
-    let current=this.head;
-    while(current !==null){
-     
-      //console.log(count)
-      if(current.data===val )
-          return true;
-      
-      current=current.next
-    }
-    return false
-  
-}
-  toString() {
-    let current = this.head;
-    let content="";
-    
-    while (current !== null) {
-      //console.log(current.data, "->");
-      content=content+`{ ${current.data} } -> `
-      current = current.next;
-    }
-    return content+'NULL'
-  }
-
-}
-
-let l1 = new linkedlist();
+let data=require('../challenges/linkedList/linked-list.js')
+let l1 = new data;
 l1.insert(1);
 l1.insert(2);
 l1.insert(5);
@@ -84,3 +26,4 @@ describe(' list include  ', () => {
         expect(l1.toString()).toStrictEqual("{ 1 } -> { 2 } -> { 5 } -> NULL");
     });
   });
+
