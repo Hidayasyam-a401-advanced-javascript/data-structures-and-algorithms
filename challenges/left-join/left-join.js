ae199393df2dd9da9a67c50bb7f19d68904226c3
HidayaSyam <hid.sy1910@gmail.com>
2020-10-21 00:33:53 +0300

    left join

---
+++ challenges/left-join/left-join.js
@@ -0,0 +1,41 @@
+'use strict'
+
+function leftJOIN(leftTable, rightTable) {
+
+  console.log(leftTable.entries);
+  let keys = [];
+  let entry = leftTable.entries;
+  let arr = [];
+ 
+  for (let i = 0; i < entry.length; i++) {
+    if (entry[i]) {
+      let current = entry[i].head
+      while (current !== null) {
+        let currentKey = Object.keys(current.data);
+        arr.push(currentKey[0])
+        current = current.next
+      }
+    }
+
+  }
+  for(let i=0;i<arr.length;i++){
+     let temp=[]
+    let leftvalue=leftTable.get(arr[i]);
+    let rightvalue=rightTable.get(arr[i]);
+    let result = rightvalue ? rightvalue[0] : null;
+    console.log('leftvalue : ',leftvalue[0],' rightvalue : ',
+    result)
+    temp.push(arr[i]);
+    temp.push(leftvalue[0]);
+    temp.push(result);
+    keys.push(temp)
+
+
+  }
+
+return keys.length
+
+
+}
+
+module.exports=leftJOIN;
